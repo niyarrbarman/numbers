5a46984f1e8203b9351b6de34c699ee1361378eb
Niyar R Barman <barmanniyar@gmail.com>
2026-02-21 00:12:22 +0100

    ddp

--- GPT2/fe/train.py
+++ GPT2/fe/train.py
@@ -49,9 +49,9 @@ gradient_accumulation_steps = 5 * 8
 batch_size = 12
 block_size = 256
 # model
-n_layer = 11
+n_layer = 12
 n_head = 8
-n_embd = 768
+n_embd = 256
 dropout = 0.0
 bias = False
 # number embedding
